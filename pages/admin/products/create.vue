f1b2432cd3cef1ffd6a7a38423dc050b39a420ec
isa <24robi@siswa.smktiannajiyah.sch.id>
2026-05-25 09:42:50 +0700

    fix pt2

--- pages/admin/products/create.vue
+++ pages/admin/products/create.vue
@@ -138,9 +138,9 @@
 import { ref, computed, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
-import { useProductsStore } from '../../stores/productsStore'
-import { useCategoriesStore } from '../../stores/categoriesStore'
-import { sGetById } from '../../services/productsService'
+import { useProductsStore } from '../../../stores/productsStore'
+import { useCategoriesStore } from '../../../stores/categoriesStore'
+import { sGetById } from '../../../services/productsService'
 
 const route = useRoute()
 const router = useRouter()
